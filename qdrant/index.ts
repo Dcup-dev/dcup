d0f97d6c8c9d354096efc60f76798ae25c220a7b
aliamerj <aliamer19ali@gmail.com>
2025-04-24 13:28:51 +0300

    add password to qdrant

--- qdrant/index.ts
+++ qdrant/index.ts
@@ -2,8 +2,7 @@ import { QdrantClient } from "@qdrant/js-client-rest";
 
 
 export const qdrant_collection_name = "documents";
-export const qdrantCLient = new QdrantClient({ url: process.env.QDRANT_DB_URL! });
-
+export const qdrantCLient = new QdrantClient({ url: process.env.QDRANT_DB_URL!, apiKey: process.env.QDRANT_DB_KEY });
 
 if (process.env.NEXT_PHASE !== 'phase-production-build') {
   const { collections } = await qdrantCLient.getCollections();
