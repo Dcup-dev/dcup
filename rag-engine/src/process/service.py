5da032014d27b7406d97cd0260be6c36f3bb9255
Ali Amer <76897266+aliamerj@users.noreply.github.com>
2026-02-15 00:21:07 +0300

    adding embedding (#126)

--- rag-engine/src/process/service.py
+++ rag-engine/src/process/service.py
@@ -1,5 +1,6 @@
 import logging
 from src.layers.chunking.chunk_document import chunk_document
+from src.layers.chunking.embedding import embed_chunks
 from src.layers.data_extractor import extractor
 from src.layers.structure_analyzer.analyzer import analyze_layout
 
@@ -15,6 +16,7 @@ def processFile(fileType: models.FileType, file_bytes: bytes, metadata: dict):
             structured_document, extractor_meta | metadata, max_tokens=400
         )
         logging.info(f"pdf data extracted pages: {len(pages)}")
+        chunks = embed_chunks(chunks)
         return [chunk.model_dump() for chunk in chunks]
 
     raise Exception("Unspported File type")
