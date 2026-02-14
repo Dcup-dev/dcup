da7e6ed318e1d09a431eff05af1c3acdf90841ab
Ali Amer <76897266+aliamerj@users.noreply.github.com>
2026-02-14 22:38:08 +0300

    Remove d engine (#125)
    * improve the layers and add metadata
    
    * remove d-engine

--- d-engine/src/main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-from src.process.controller import router as process
-from .logging import configure_logging, LogLevels
-
-configure_logging(LogLevels.info)
-app = FastAPI()
-app.include_router(process)
